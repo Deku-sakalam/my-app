0fac763f03182adb251ba512a9121a849446dc83
Vincent Acosta <acostavincent46@gmail.com>
2025-11-21 21:09:56 +0800

    fix  basic comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useEffect, useState, useTransition } from "react";
 import bankerslogo from "./Icon/bankers.png";
-import createPost, { getAll, LikePost, unLikePost } from "./db";
+import createPost, {
+  commentPost,
+  DisLikePost,
+  GetPosts,
+  LikePost,
+  Post,
+} from "./db";
 
 export function App() {
   const [hidden, setHidden] = useState(true);
   const [textV, setTextV] = useState("");
-  const [onComment, setOncomment] = useState("commentMessage closed");
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [com, setCom] = useState("");
+  const [textbox, setTextbox] = useState<string | undefined>();
+  const [id, setID] = useState(crypto.randomUUID());
+
+  useEffect(() => {
+    setPosts(GetPosts());
+  }, []);
+
   useEffect(() => {
     if (!textV) {
       setHidden(true);
@@ -35,9 +49,10 @@ export function App() {
             <button
               onClick={() => {
                 if (textV) {
-                  createPost(textV);
+                  const newPost = createPost(textV);
+                  setTextV("");
+                  setPosts(newPost);
                 }
-                setTextV("");
               }}
             >
               Post!
@@ -45,7 +60,8 @@ export function App() {
           </div>
         </div>
         <div className="containerMessages">
-          {getAll().map((post: any) => {
+          {[...posts].reverse().map((post: Post) => {
+            const comments = post.comments;
             const formattedDate = new Date(post.date).toLocaleString("en-US", {
               month: "short",
               day: "numeric",
@@ -59,12 +75,69 @@ export function App() {
                   <div className="userName">{formattedDate}</div>
                   <div className="message">{post.content}</div>
                   <div className="action">
-                    <button>👍like</button>
-                    <button>👎unlike</button>
-                    <button onClick={() => {}}>💬Comment</button>
+                    <button
+                      onClick={() => {
+                        const newPosts = LikePost(post.id);
+                        setPosts(newPosts);
+                      }}
+                    >
+                      {post.like.toString()}👍like
+                    </button>
+                    <button
+                      onClick={() => {
+                        const newPosts = DisLikePost(post.id);
+                        setPosts(newPosts);
+                      }}
+                    >
+                      {post.disLike.toString()}👎dislike
+                    </button>
+                    <button
+                      onClick={() => {
+                        setTextbox(post.id);
+                        setCom("");
+                      }}
+                    >
+                      💬Comment
+                    </button>
                     <button>➣Share</button>
                   </div>
-                  <div hidden={hidden} className={onComment}></div>
+                  <div className={textbox === post.id ? "textbox" : "closed"}>
+                    <textarea
+                      value={com}
+                      onChange={(e) => {
+                        setCom(e.target.value);
+                      }}
+                    ></textarea>
+                    <button
+                      onClick={() => {
+                        const newPost = commentPost(post.id, com);
+                        setPosts(newPost);
+                        setTextbox(undefined);
+                      }}
+                    >
+                      submit
+                    </button>
+                  </div>
+                  <div>
+                    {[...comments].reverse().map((id) => {
+                      const commentDate = new Date(id.date).toLocaleString(
+                        "en-US",
+                        {
+                          month: "short",
+                          day: "numeric",
+                          year: "numeric",
+                          hour: "numeric",
+                          minute: "2-digit",
+                        }
+                      );
+                      return (
+                        <div className="">
+                          <div className="userName">{commentDate}</div>
+                          <div className="message">{id.content}</div>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
             );
--- src/db.ts
+++ src/db.ts
@@ -1,65 +1,67 @@
 const POSTKEY = "content";
-//create post
-//paramiter data with content
-//get all
+export type Post = {
+  id: string;
+  content: string;
+  like: number;
+  disLike: number;
+  date: Date;
+  comments: {
+    id: string;
+    content: string;
+    date: Date;
+  }[];
+};
+
 function createPost(content: string) {
   const lStorage = localStorage.getItem(POSTKEY);
   const jLStorage = lStorage ? JSON.parse(lStorage) : [];
-
   jLStorage.push({
+    id: crypto.randomUUID(),
     content: content,
     date: new Date(),
+    like: 0,
+    disLike: 0,
+    comments: [],
   });
   localStorage.setItem(POSTKEY, JSON.stringify(jLStorage));
+  return jLStorage;
 }
 
-//like post with id
-function likePost(id: string) {
-  const lStorage = localStorage.getItem(POSTKEY);
-  if (lStorage) {
-    const jLStorage = JSON.parse(lStorage) ?? [];
-    jLStorage.push({
-      id: id,
-    });
-    localStorage.setItem(POSTKEY, JSON.stringify(jLStorage));
-  }
-}
-export function getAll() {
+export function GetPosts() {
   const lStorage = localStorage.getItem(POSTKEY);
   const jLStorage = lStorage ? JSON.parse(lStorage) : [];
   return jLStorage;
 }
-//unlike post with id
+
 export function LikePost(id: string) {
   const lStorage = localStorage.getItem(POSTKEY);
-  if (lStorage) {
-    const jLStorage = JSON.parse(lStorage) ?? [];
-    jLStorage.push({
-      id: id,
-    });
-    localStorage.setItem(POSTKEY, JSON.stringify(jLStorage));
-  }
+  const posts = lStorage ? JSON.parse(lStorage) : [];
+  const index = posts.findIndex((post: Post) => post.id === id);
+  posts[index].like++;
+  localStorage.setItem(POSTKEY, JSON.stringify(posts));
+  return posts;
 }
-export function unLikePost(id: string) {
+
+export function DisLikePost(id: string) {
   const lStorage = localStorage.getItem(POSTKEY);
-  if (lStorage) {
-    const jLStorage = JSON.parse(lStorage) ?? [];
-    jLStorage.push({
-      id: id,
-    });
-    localStorage.setItem(POSTKEY, JSON.stringify(jLStorage));
-  }
+  const posts = lStorage ? JSON.parse(lStorage) : [];
+  const index = posts.findIndex((post: Post) => post.id === id);
+  posts[index].disLike++;
+  localStorage.setItem(POSTKEY, JSON.stringify(posts));
+  return posts;
 }
-//comment post with paramiter id and content
-function commentPost(id: string, content: string) {
+
+export function commentPost(id: string, content: string) {
   const lStorage = localStorage.getItem(POSTKEY);
-  if (lStorage) {
-    const jLStorage = JSON.parse(lStorage) ?? [];
-    jLStorage.push({
-      id: id,
-      content: content,
-    });
-    localStorage.setItem(POSTKEY, JSON.stringify(jLStorage));
-  }
+  const posts = lStorage ? JSON.parse(lStorage) : [];
+  const index = posts.findIndex((posts: Post) => posts.id === id);
+  posts[index].comments.push({
+    id: crypto.randomUUID(),
+    content: content,
+    date: new Date(),
+  });
+  localStorage.setItem(POSTKEY, JSON.stringify(posts));
+  return posts;
 }
+
 export default createPost;
